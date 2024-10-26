7805011275a42470cebd4077fa33e34b1336e317
Md Milon <abireshan32@gmail.com>
2024-10-26 15:45:21 +0600

    add prodcut to local storage

--- js/index.js
+++ js/index.js
@@ -2,3 +2,21 @@ function toggleDrawer() {
     const drawer = document.getElementById('drawer');
     drawer.classList.toggle('translate-x-full');
 }
+
+function addToCart(){
+    const productName=document.getElementById('productName').innerText
+const productPrice=document.getElementById('productPrice').innerText
+const productImage=document.getElementById('productImage').src
+
+const product={
+    name:productName,
+    price:productPrice,
+    image:productImage
+}
+console.log(product);
+
+let cart=JSON.parse(localStorage.getItem('cart'))||[]
+cart.push(product)
+localStorage.setItem('cart',JSON.stringify(cart))
+alert(`${productName} added to your cart`)
+}
